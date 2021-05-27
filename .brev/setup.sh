4a9d54375b747f52af39baedcf12a77b5a414777
Drew Malin <drewmalin@gmail.com>
2021-05-27 14:28:53 -0700

    zsh

---
+++ .brev/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+sudo apt-get install zsh -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "zsh" >> ~/.bashrc
